3468c6564f9ea7849cc8e9b66f1d8acb2652a8c2
arunkanil1994@gmail.com <arunkanil1994@gmail.com>
2022-02-09 17:45:20 +0530

    first commit

---
+++ build/precache-manifest.0b291b0f8c07f37301bb867650d6b9bd.js
@@ -0,0 +1,374 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea575275dd8132570bf51ecb69978a9",
+    "url": "/holiday/index.html"
+  },
+  {
+    "revision": "f1c711e8554a67f52d50",
+    "url": "/holiday/static/css/0.de1c6f3c.chunk.css"
+  },
+  {
+    "revision": "326fd36da322327baed5",
+    "url": "/holiday/static/css/10.338b5900.chunk.css"
+  },
+  {
+    "revision": "96db509f776f17163544",
+    "url": "/holiday/static/css/11.2e8a4633.chunk.css"
+  },
+  {
+    "revision": "d66fb30a007f43275054",
+    "url": "/holiday/static/css/12.b830de30.chunk.css"
+  },
+  {
+    "revision": "600a5b6322f676766cd9",
+    "url": "/holiday/static/css/13.a1e78890.chunk.css"
+  },
+  {
+    "revision": "bb4ea8b709bb84dbfe9d",
+    "url": "/holiday/static/css/14.adf815cd.chunk.css"
+  },
+  {
+    "revision": "22be436ed5fd511ba25e",
+    "url": "/holiday/static/css/15.d7f9e97d.chunk.css"
+  },
+  {
+    "revision": "e4781edf716421b8795f",
+    "url": "/holiday/static/css/16.0b75bb7d.chunk.css"
+  },
+  {
+    "revision": "95e4ce51a095cef359e4",
+    "url": "/holiday/static/css/2.ae59248a.chunk.css"
+  },
+  {
+    "revision": "cde0d3cb08c2b03686d3",
+    "url": "/holiday/static/css/20.819092da.chunk.css"
+  },
+  {
+    "revision": "f5ceef02f1b4d400318a",
+    "url": "/holiday/static/css/27.819092da.chunk.css"
+  },
+  {
+    "revision": "a2725355b3fbb2803f78",
+    "url": "/holiday/static/css/28.2293a5c2.chunk.css"
+  },
+  {
+    "revision": "7439ed42364041cc22e3",
+    "url": "/holiday/static/css/29.841bc42d.chunk.css"
+  },
+  {
+    "revision": "71a41e00a34f59ab5195",
+    "url": "/holiday/static/css/3.1892d553.chunk.css"
+  },
+  {
+    "revision": "514eaf49c81b3e209ff9",
+    "url": "/holiday/static/css/30.efab397b.chunk.css"
+  },
+  {
+    "revision": "a718e7376f2ef9062c59",
+    "url": "/holiday/static/css/31.efab397b.chunk.css"
+  },
+  {
+    "revision": "8f3b8a73832573bb1fea",
+    "url": "/holiday/static/css/32.efab397b.chunk.css"
+  },
+  {
+    "revision": "92cd91e75a84dc4753ad",
+    "url": "/holiday/static/css/33.908d57c2.chunk.css"
+  },
+  {
+    "revision": "e7b36610321fa4a51d95",
+    "url": "/holiday/static/css/34.5b5d68f8.chunk.css"
+  },
+  {
+    "revision": "14f70b30b32aa84d9c6f",
+    "url": "/holiday/static/css/35.8a4f5277.chunk.css"
+  },
+  {
+    "revision": "dd7ee791bf5136b00f6f",
+    "url": "/holiday/static/css/36.befa28b4.chunk.css"
+  },
+  {
+    "revision": "fdc8abee1b350758a77c",
+    "url": "/holiday/static/css/4.8582e097.chunk.css"
+  },
+  {
+    "revision": "3dc42ba6b82c014014bc",
+    "url": "/holiday/static/css/6.21298d47.chunk.css"
+  },
+  {
+    "revision": "378dab9ef2c78e3f5705",
+    "url": "/holiday/static/css/main.0251a874.chunk.css"
+  },
+  {
+    "revision": "f1c711e8554a67f52d50",
+    "url": "/holiday/static/js/0.68d42471.chunk.js"
+  },
+  {
+    "revision": "ff878ec18a39d8774e46",
+    "url": "/holiday/static/js/1.258cea1f.chunk.js"
+  },
+  {
+    "revision": "326fd36da322327baed5",
+    "url": "/holiday/static/js/10.64116211.chunk.js"
+  },
+  {
+    "revision": "39479d911a726fb8a97eb8dbf747ab4f",
+    "url": "/holiday/static/js/10.64116211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96db509f776f17163544",
+    "url": "/holiday/static/js/11.033e86c5.chunk.js"
+  },
+  {
+    "revision": "d66fb30a007f43275054",
+    "url": "/holiday/static/js/12.c08ad1a2.chunk.js"
+  },
+  {
+    "revision": "600a5b6322f676766cd9",
+    "url": "/holiday/static/js/13.af0d6a5e.chunk.js"
+  },
+  {
+    "revision": "bb4ea8b709bb84dbfe9d",
+    "url": "/holiday/static/js/14.6f8a3e78.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday/static/js/14.6f8a3e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22be436ed5fd511ba25e",
+    "url": "/holiday/static/js/15.805ed642.chunk.js"
+  },
+  {
+    "revision": "e4781edf716421b8795f",
+    "url": "/holiday/static/js/16.574dbe75.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday/static/js/16.574dbe75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859d863fd793b0704c03",
+    "url": "/holiday/static/js/17.f5dc7bba.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday/static/js/17.f5dc7bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eab48f19a6bcbdee852",
+    "url": "/holiday/static/js/18.2e426142.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday/static/js/18.2e426142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b2a391cdd3639c9bb7",
+    "url": "/holiday/static/js/19.7722cdb9.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday/static/js/19.7722cdb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e4ce51a095cef359e4",
+    "url": "/holiday/static/js/2.a87ca84d.chunk.js"
+  },
+  {
+    "revision": "cde0d3cb08c2b03686d3",
+    "url": "/holiday/static/js/20.8345b2b6.chunk.js"
+  },
+  {
+    "revision": "188013a65c3ef8b26428",
+    "url": "/holiday/static/js/21.3c7dce69.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday/static/js/21.3c7dce69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aed26bd4228228640a0",
+    "url": "/holiday/static/js/22.357b0adb.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday/static/js/22.357b0adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b971e9b61780f313f7",
+    "url": "/holiday/static/js/23.96a4e202.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday/static/js/23.96a4e202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "195a6e4ac69f92d47bf0",
+    "url": "/holiday/static/js/24.5f29c136.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday/static/js/24.5f29c136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a2ca039d43cd41e80c",
+    "url": "/holiday/static/js/25.ab99b940.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday/static/js/25.ab99b940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850b5b8d7c98d7765777",
+    "url": "/holiday/static/js/26.fc81ea08.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday/static/js/26.fc81ea08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ceef02f1b4d400318a",
+    "url": "/holiday/static/js/27.19fb53b6.chunk.js"
+  },
+  {
+    "revision": "a2725355b3fbb2803f78",
+    "url": "/holiday/static/js/28.f0227eb9.chunk.js"
+  },
+  {
+    "revision": "7439ed42364041cc22e3",
+    "url": "/holiday/static/js/29.d5990809.chunk.js"
+  },
+  {
+    "revision": "71a41e00a34f59ab5195",
+    "url": "/holiday/static/js/3.1fceafb7.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday/static/js/3.1fceafb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514eaf49c81b3e209ff9",
+    "url": "/holiday/static/js/30.90799a3e.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday/static/js/30.90799a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a718e7376f2ef9062c59",
+    "url": "/holiday/static/js/31.b73c21c3.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday/static/js/31.b73c21c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f3b8a73832573bb1fea",
+    "url": "/holiday/static/js/32.dd2e3db2.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday/static/js/32.dd2e3db2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92cd91e75a84dc4753ad",
+    "url": "/holiday/static/js/33.ea3d4358.chunk.js"
+  },
+  {
+    "revision": "e7b36610321fa4a51d95",
+    "url": "/holiday/static/js/34.3280529e.chunk.js"
+  },
+  {
+    "revision": "14f70b30b32aa84d9c6f",
+    "url": "/holiday/static/js/35.489b8965.chunk.js"
+  },
+  {
+    "revision": "dd7ee791bf5136b00f6f",
+    "url": "/holiday/static/js/36.5217994b.chunk.js"
+  },
+  {
+    "revision": "1b76e87cdfb5e996ebfc",
+    "url": "/holiday/static/js/37.3ff8e159.chunk.js"
+  },
+  {
+    "revision": "fdc8abee1b350758a77c",
+    "url": "/holiday/static/js/4.52c714e3.chunk.js"
+  },
+  {
+    "revision": "6623fab25d97d87fc8d6",
+    "url": "/holiday/static/js/5.5141c33b.chunk.js"
+  },
+  {
+    "revision": "3dc42ba6b82c014014bc",
+    "url": "/holiday/static/js/6.1c53864b.chunk.js"
+  },
+  {
+    "revision": "1317e81d2465e1809aa1",
+    "url": "/holiday/static/js/7.ae9f9181.chunk.js"
+  },
+  {
+    "revision": "378dab9ef2c78e3f5705",
+    "url": "/holiday/static/js/main.b638ada9.chunk.js"
+  },
+  {
+    "revision": "8e39a8b805ff4304b1f6",
+    "url": "/holiday/static/js/runtime-main.3d76cc08.js"
+  },
+  {
+    "revision": "10ebf9189344922639b131e345327395",
+    "url": "/holiday/static/media/401.10ebf918.png"
+  },
+  {
+    "revision": "6197a0901ba6b21a860b2327e138eba6",
+    "url": "/holiday/static/media/404.6197a090.png"
+  },
+  {
+    "revision": "9a1bd4534302f92ea3b997ad76f1ec1d",
+    "url": "/holiday/static/media/500.9a1bd453.png"
+  },
+  {
+    "revision": "1bcdb5d5b62188b6df23903800ca5d96",
+    "url": "/holiday/static/media/ArcelorMittal-01.1bcdb5d5.svg"
+  },
+  {
+    "revision": "0facaae97183b8fede52099930aefd8d",
+    "url": "/holiday/static/media/HelveticaNeueLight.0facaae9.ttf"
+  },
+  {
+    "revision": "348dee448f78d73d826c49b0c1e89f6e",
+    "url": "/holiday/static/media/abp-logo-long.348dee44.png"
+  },
+  {
+    "revision": "fd7c100fdd5537febf41cf83a32cb11c",
+    "url": "/holiday/static/media/approval.fd7c100f.svg"
+  },
+  {
+    "revision": "2999d7dbc9597d512bebbe85cc796275",
+    "url": "/holiday/static/media/deleteIcon.2999d7db.svg"
+  },
+  {
+    "revision": "2ab221362ac37906a8c6d998093d509e",
+    "url": "/holiday/static/media/edit.2ab22136.svg"
+  },
+  {
+    "revision": "db69a827c1ee51b6b74c0d922e56513f",
+    "url": "/holiday/static/media/empdata.db69a827.xlsx"
+  },
+  {
+    "revision": "0502e76342919a809bd6c72973c8ca3a",
+    "url": "/holiday/static/media/microsoft.0502e763.svg"
+  },
+  {
+    "revision": "c1ff4dc97144b1b33b16dd04d9127e06",
+    "url": "/holiday/static/media/user.c1ff4dc9.svg"
+  },
+  {
+    "revision": "4c29894eb4a9fb1265d30ed8eb995fda",
+    "url": "/holiday/static/media/userLayout-Backround.4c29894e.svg"
+  },
+  {
+    "revision": "eecc431ec418863b91287aece712b12c",
+    "url": "/holiday/static/media/web.eecc431e.config"
+  }
+]);
