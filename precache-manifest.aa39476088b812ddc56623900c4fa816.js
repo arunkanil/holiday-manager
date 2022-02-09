29124866e24205eaf384686cac31ce69cc868a13
arunkanil1994@gmail.com <arunkanil1994@gmail.com>
2022-02-09 18:04:32 +0530

    Updates

---
+++ precache-manifest.aa39476088b812ddc56623900c4fa816.js
@@ -0,0 +1,374 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a5a54a9c4246c7703e20a1f6188b0f",
+    "url": "/holiday-manager/index.html"
+  },
+  {
+    "revision": "f1c711e8554a67f52d50",
+    "url": "/holiday-manager/static/css/0.de1c6f3c.chunk.css"
+  },
+  {
+    "revision": "215bf5cf566f268bbeea",
+    "url": "/holiday-manager/static/css/10.338b5900.chunk.css"
+  },
+  {
+    "revision": "d1e0d8ed555c413c70a4",
+    "url": "/holiday-manager/static/css/11.2e8a4633.chunk.css"
+  },
+  {
+    "revision": "972dd665e2d816857f2f",
+    "url": "/holiday-manager/static/css/12.b830de30.chunk.css"
+  },
+  {
+    "revision": "498ea5b9eeee747b3c7d",
+    "url": "/holiday-manager/static/css/13.a1e78890.chunk.css"
+  },
+  {
+    "revision": "68180f57262753b01f3d",
+    "url": "/holiday-manager/static/css/14.adf815cd.chunk.css"
+  },
+  {
+    "revision": "22be436ed5fd511ba25e",
+    "url": "/holiday-manager/static/css/15.d7f9e97d.chunk.css"
+  },
+  {
+    "revision": "e8c417ad6759ef8369f2",
+    "url": "/holiday-manager/static/css/16.73dc662e.chunk.css"
+  },
+  {
+    "revision": "95e4ce51a095cef359e4",
+    "url": "/holiday-manager/static/css/2.ae59248a.chunk.css"
+  },
+  {
+    "revision": "9cb8bf38243d3f27ab0f",
+    "url": "/holiday-manager/static/css/20.819092da.chunk.css"
+  },
+  {
+    "revision": "6aef3e9f00b25ad3cd53",
+    "url": "/holiday-manager/static/css/27.819092da.chunk.css"
+  },
+  {
+    "revision": "1dfc19bd33eea85918be",
+    "url": "/holiday-manager/static/css/28.7d20d8e5.chunk.css"
+  },
+  {
+    "revision": "7439ed42364041cc22e3",
+    "url": "/holiday-manager/static/css/29.841bc42d.chunk.css"
+  },
+  {
+    "revision": "71a41e00a34f59ab5195",
+    "url": "/holiday-manager/static/css/3.1892d553.chunk.css"
+  },
+  {
+    "revision": "0c2a03877758a828ee03",
+    "url": "/holiday-manager/static/css/30.efab397b.chunk.css"
+  },
+  {
+    "revision": "d535f84dae5eb29bff95",
+    "url": "/holiday-manager/static/css/31.efab397b.chunk.css"
+  },
+  {
+    "revision": "be077c3252d89ec4f4bb",
+    "url": "/holiday-manager/static/css/32.efab397b.chunk.css"
+  },
+  {
+    "revision": "08752749401ecc40d6b9",
+    "url": "/holiday-manager/static/css/33.908d57c2.chunk.css"
+  },
+  {
+    "revision": "559b3d9dc96c7bbc5c26",
+    "url": "/holiday-manager/static/css/34.3230f1f3.chunk.css"
+  },
+  {
+    "revision": "4dc12f4b237fd1ea6a46",
+    "url": "/holiday-manager/static/css/35.8a4f5277.chunk.css"
+  },
+  {
+    "revision": "a56b3178440a99d64794",
+    "url": "/holiday-manager/static/css/36.befa28b4.chunk.css"
+  },
+  {
+    "revision": "fdc8abee1b350758a77c",
+    "url": "/holiday-manager/static/css/4.8582e097.chunk.css"
+  },
+  {
+    "revision": "32239ca740cdcb2dd053",
+    "url": "/holiday-manager/static/css/6.21298d47.chunk.css"
+  },
+  {
+    "revision": "93b523cce8dd20d701b8",
+    "url": "/holiday-manager/static/css/main.8920c87f.chunk.css"
+  },
+  {
+    "revision": "f1c711e8554a67f52d50",
+    "url": "/holiday-manager/static/js/0.68d42471.chunk.js"
+  },
+  {
+    "revision": "ff878ec18a39d8774e46",
+    "url": "/holiday-manager/static/js/1.258cea1f.chunk.js"
+  },
+  {
+    "revision": "215bf5cf566f268bbeea",
+    "url": "/holiday-manager/static/js/10.7a7d00e3.chunk.js"
+  },
+  {
+    "revision": "39479d911a726fb8a97eb8dbf747ab4f",
+    "url": "/holiday-manager/static/js/10.7a7d00e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e0d8ed555c413c70a4",
+    "url": "/holiday-manager/static/js/11.846b19ce.chunk.js"
+  },
+  {
+    "revision": "972dd665e2d816857f2f",
+    "url": "/holiday-manager/static/js/12.c34c83b7.chunk.js"
+  },
+  {
+    "revision": "498ea5b9eeee747b3c7d",
+    "url": "/holiday-manager/static/js/13.9c2f90df.chunk.js"
+  },
+  {
+    "revision": "68180f57262753b01f3d",
+    "url": "/holiday-manager/static/js/14.93048849.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-manager/static/js/14.93048849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22be436ed5fd511ba25e",
+    "url": "/holiday-manager/static/js/15.805ed642.chunk.js"
+  },
+  {
+    "revision": "e8c417ad6759ef8369f2",
+    "url": "/holiday-manager/static/js/16.55f160df.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday-manager/static/js/16.55f160df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a307c5e89419b5fe8b85",
+    "url": "/holiday-manager/static/js/17.4cf881e1.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-manager/static/js/17.4cf881e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616b2b6d85c19b2123f4",
+    "url": "/holiday-manager/static/js/18.4c48fc73.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-manager/static/js/18.4c48fc73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1692db293d6806f571d9",
+    "url": "/holiday-manager/static/js/19.820d1899.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-manager/static/js/19.820d1899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e4ce51a095cef359e4",
+    "url": "/holiday-manager/static/js/2.a87ca84d.chunk.js"
+  },
+  {
+    "revision": "9cb8bf38243d3f27ab0f",
+    "url": "/holiday-manager/static/js/20.4fb6fd58.chunk.js"
+  },
+  {
+    "revision": "8e9304ecbcebb5bd8ae2",
+    "url": "/holiday-manager/static/js/21.ab077500.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-manager/static/js/21.ab077500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1ad09bdb4f4c98bc0e",
+    "url": "/holiday-manager/static/js/22.69a1454b.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-manager/static/js/22.69a1454b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030cce22376ae5ace1f6",
+    "url": "/holiday-manager/static/js/23.a248fe7b.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-manager/static/js/23.a248fe7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675d2551f519d4f53582",
+    "url": "/holiday-manager/static/js/24.76e94ad8.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-manager/static/js/24.76e94ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f81d1d72982c66505bc",
+    "url": "/holiday-manager/static/js/25.3b1e95a2.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-manager/static/js/25.3b1e95a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d415ba7e11805c47f9",
+    "url": "/holiday-manager/static/js/26.d79ffaca.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-manager/static/js/26.d79ffaca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aef3e9f00b25ad3cd53",
+    "url": "/holiday-manager/static/js/27.7720e757.chunk.js"
+  },
+  {
+    "revision": "1dfc19bd33eea85918be",
+    "url": "/holiday-manager/static/js/28.69f236f7.chunk.js"
+  },
+  {
+    "revision": "7439ed42364041cc22e3",
+    "url": "/holiday-manager/static/js/29.d5990809.chunk.js"
+  },
+  {
+    "revision": "71a41e00a34f59ab5195",
+    "url": "/holiday-manager/static/js/3.1fceafb7.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday-manager/static/js/3.1fceafb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2a03877758a828ee03",
+    "url": "/holiday-manager/static/js/30.c24f2fed.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday-manager/static/js/30.c24f2fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d535f84dae5eb29bff95",
+    "url": "/holiday-manager/static/js/31.c51a9e79.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday-manager/static/js/31.c51a9e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be077c3252d89ec4f4bb",
+    "url": "/holiday-manager/static/js/32.3acce678.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday-manager/static/js/32.3acce678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08752749401ecc40d6b9",
+    "url": "/holiday-manager/static/js/33.55aeaafd.chunk.js"
+  },
+  {
+    "revision": "559b3d9dc96c7bbc5c26",
+    "url": "/holiday-manager/static/js/34.97982438.chunk.js"
+  },
+  {
+    "revision": "4dc12f4b237fd1ea6a46",
+    "url": "/holiday-manager/static/js/35.0531950e.chunk.js"
+  },
+  {
+    "revision": "a56b3178440a99d64794",
+    "url": "/holiday-manager/static/js/36.fe0f0823.chunk.js"
+  },
+  {
+    "revision": "5c6c68763780bb83fc0d",
+    "url": "/holiday-manager/static/js/37.33495b03.chunk.js"
+  },
+  {
+    "revision": "fdc8abee1b350758a77c",
+    "url": "/holiday-manager/static/js/4.52c714e3.chunk.js"
+  },
+  {
+    "revision": "6623fab25d97d87fc8d6",
+    "url": "/holiday-manager/static/js/5.5141c33b.chunk.js"
+  },
+  {
+    "revision": "32239ca740cdcb2dd053",
+    "url": "/holiday-manager/static/js/6.73d79127.chunk.js"
+  },
+  {
+    "revision": "1317e81d2465e1809aa1",
+    "url": "/holiday-manager/static/js/7.ae9f9181.chunk.js"
+  },
+  {
+    "revision": "93b523cce8dd20d701b8",
+    "url": "/holiday-manager/static/js/main.1e4129d0.chunk.js"
+  },
+  {
+    "revision": "296555bb8f394bc03932",
+    "url": "/holiday-manager/static/js/runtime-main.326f5038.js"
+  },
+  {
+    "revision": "10ebf9189344922639b131e345327395",
+    "url": "/holiday-manager/static/media/401.10ebf918.png"
+  },
+  {
+    "revision": "6197a0901ba6b21a860b2327e138eba6",
+    "url": "/holiday-manager/static/media/404.6197a090.png"
+  },
+  {
+    "revision": "9a1bd4534302f92ea3b997ad76f1ec1d",
+    "url": "/holiday-manager/static/media/500.9a1bd453.png"
+  },
+  {
+    "revision": "1bcdb5d5b62188b6df23903800ca5d96",
+    "url": "/holiday-manager/static/media/ArcelorMittal-01.1bcdb5d5.svg"
+  },
+  {
+    "revision": "0facaae97183b8fede52099930aefd8d",
+    "url": "/holiday-manager/static/media/HelveticaNeueLight.0facaae9.ttf"
+  },
+  {
+    "revision": "348dee448f78d73d826c49b0c1e89f6e",
+    "url": "/holiday-manager/static/media/abp-logo-long.348dee44.png"
+  },
+  {
+    "revision": "fd7c100fdd5537febf41cf83a32cb11c",
+    "url": "/holiday-manager/static/media/approval.fd7c100f.svg"
+  },
+  {
+    "revision": "2999d7dbc9597d512bebbe85cc796275",
+    "url": "/holiday-manager/static/media/deleteIcon.2999d7db.svg"
+  },
+  {
+    "revision": "2ab221362ac37906a8c6d998093d509e",
+    "url": "/holiday-manager/static/media/edit.2ab22136.svg"
+  },
+  {
+    "revision": "db69a827c1ee51b6b74c0d922e56513f",
+    "url": "/holiday-manager/static/media/empdata.db69a827.xlsx"
+  },
+  {
+    "revision": "0502e76342919a809bd6c72973c8ca3a",
+    "url": "/holiday-manager/static/media/microsoft.0502e763.svg"
+  },
+  {
+    "revision": "c1ff4dc97144b1b33b16dd04d9127e06",
+    "url": "/holiday-manager/static/media/user.c1ff4dc9.svg"
+  },
+  {
+    "revision": "4c29894eb4a9fb1265d30ed8eb995fda",
+    "url": "/holiday-manager/static/media/userLayout-Backround.4c29894e.svg"
+  },
+  {
+    "revision": "eecc431ec418863b91287aece712b12c",
+    "url": "/holiday-manager/static/media/web.eecc431e.config"
+  }
+]);
